7f9c4493e7cec4753bd78ad1c4aa17bd13a424c1
Kavya Naik <kavyanaik2112@gmail.com>
2022-04-11 16:15:32 +0530

    first commit

--- CP/codeforces/1.cpp
+++ CP/codeforces/1.cpp
@@ -7,10 +7,22 @@ int32_t main()
     int t; cin>>t;
     while(t--){
         int n; cin>>n;
-        vector<int>v(n);
-        for(auto &u:v) cin>>u;
-        for(auto &u:v) cout<<u<<" ";
-        cout<<endl;
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+            int temp; cin>>temp;
+            mp[temp]++;
+        }
+        int max=0;
+        for(auto u:mp){
+            if(u.second>max) max=u.second;
+        }
+        int ans=0;
+        while((n-max)>0){
+            ans+=min(n-max,max);
+            ans++;
+            max*=2;
+        }
+        cout<<ans<<endl;
     }
     return 0;
 }
